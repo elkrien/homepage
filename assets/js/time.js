9ae7db7cc0f9bb36daadb7ca20403cb50028dc0c
elkrien <elkrien@gmail.com>
2023-02-11 20:42:35 +0100

    first commit

---
+++ assets/js/time.js
@@ -0,0 +1,31 @@
+// ┌┬┐┬┌┬┐┌─┐
+//  │ ││││├┤
+//  ┴ ┴┴ ┴└─┘
+// Set time and Date
+
+window.onload = displayClock();
+function displayClock() {
+	const monthNames = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
+
+	var d = new Date();
+	var mm = '\xa0' + monthNames[d.getMonth()];
+	var dd = d.getDate();
+	var min = (mins = ('0' + d.getMinutes()).slice(-2));
+	var hh = d.getHours();
+	var ampm = '';
+
+	if (CONFIG.twelveHourFormat) {
+		ampm = hh >= 12 ? ' pm' : ' am';
+		hh = hh % 12;
+		hh = hh ? hh : 12;
+	}
+
+	document.getElementById('hour').innerText = hh;
+	document.getElementById('separator').innerHTML = ' : ';
+	document.getElementById('minutes').innerText = min + ampm;
+
+	document.getElementById('month').innerText = mm;
+	document.getElementById('day').innerText = dd;
+
+	setTimeout(displayClock, 1000);
+}
---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Maciej',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Dzień Dobry!',
+	greetingAfternoon: 'Dzień Dobry,',
+	greetingEvening: 'Dobry Wieczór,',
+	greetingNight: 'Pora iść spać!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '2b19a7c68543a30e31f14cbdb52555d6', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'pl', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '50.033779',
+	defaultLongitude: '22.047470',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/elkrien?tab=repositories',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://gmail.com/',
+		},
+		{
+			id: '3',
+			name: 'Discord',
+			icon: 'bot',
+			link: 'https://discord.com/channels/@me',
+		},
+		{
+			id: '4',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '5',
+			name: 'YouTube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
+		},
+		{
+			id: '6',
+			name: 'Udemy',
+			icon: 'graduation-cap',
+			link: 'https://udemy.com',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'newspaper',
+			id: '1',
+			links: [
+				{
+					name: 'RMF24',
+					link: 'https://www.rmf24.pl/',
+				},
+				{
+					name: "Spider's Web",
+					link: 'https://spidersweb.pl/',
+				},
+				{
+					name: 'Antyweb',
+					link: 'https://antyweb.pl/',
+				},
+				{
+					name: 'LXer',
+					link: 'http://lxer.com/',
+				},
+			],
+		},
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Allegro',
+					link: 'https://allegro.pl',
+				},
+				{
+					name: 'naEKRANIE',
+					link: 'https://naekranie.pl/',
+				},
+				{
+					name: 'Monkeytype',
+					link: 'https://monkeytype.com/',
+				},
+				{
+					name: 'Pepper',
+					link: 'https://www.pepper.pl/nowe',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
